19daa49814aa1def77079b8d73f8de6476e46393
Srinivasa Sameer Addepalli <srinivasaddepalli13@gmail.com>
2024-11-29 14:28:42 -0500

    Profile Folder

---
+++ EventGram/Profile/ProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileView.swift
+//  EventGram
+//
+//  Created by Srinivasa Sameer Addepalli on 11/29/24.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
