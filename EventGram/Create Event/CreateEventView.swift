e93e4eab9c100f10f30729aa89e01d297a6b6563
Srinivasa Sameer Addepalli <srinivasaddepalli13@gmail.com>
2024-11-29 14:18:49 -0500

    Created New Files for all screens

---
+++ EventGram/Create Event/CreateEventView.swift
@@ -0,0 +1,20 @@
+//
+//  CreateEventView.swift
+//  EventGram
+//
+//  Created by Srinivasa Sameer Addepalli on 11/29/24.
+//
+
+import UIKit
+
+class CreateEventView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ EventGram/Event Details/EventDetailsView.swift
@@ -0,0 +1,20 @@
+//
+//  EventDetailsView.swift
+//  EventGram
+//
+//  Created by Srinivasa Sameer Addepalli on 11/29/24.
+//
+
+import UIKit
+
+class EventDetailsView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
