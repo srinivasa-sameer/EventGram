fbf8bcf1af811657b02af5a0b6c7965d488987b9
Vidhi Thacker <46989276+vidhit31@users.noreply.github.com>
2024-12-03 13:45:27 -0500

    Sign In

---
+++ EventGram/Home/MainScreenView.swift
@@ -0,0 +1,56 @@
+//
+//  MainScreenView.swift
+//  EventGram
+//
+//  Created by Vidhi Thacker on 11/14/24.
+//
+
+import UIKit
+
+class MainScreenView: UIView {
+    var profilePic: UIImageView!
+    var labelText: UILabel!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = .white
+        
+        setupProfilePic()
+        setupLabelText()
+        initConstraints()
+    }
+    
+    func setupProfilePic(){
+        profilePic = UIImageView()
+        profilePic.image = UIImage(systemName: "person.circle")?.withRenderingMode(.alwaysOriginal)
+        profilePic.contentMode = .scaleToFill
+        profilePic.clipsToBounds = true
+        profilePic.layer.masksToBounds = true
+        profilePic.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(profilePic)
+    }
+    
+    func setupLabelText(){
+        labelText = UILabel()
+        labelText.font = .boldSystemFont(ofSize: 14)
+        labelText.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(labelText)
+    }
+    
+    func initConstraints(){
+        NSLayoutConstraint.activate([
+            profilePic.widthAnchor.constraint(equalToConstant: 32),
+            profilePic.heightAnchor.constraint(equalToConstant: 32),
+            profilePic.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 8),
+            profilePic.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            
+            labelText.topAnchor.constraint(equalTo: profilePic.topAnchor),
+            labelText.bottomAnchor.constraint(equalTo: profilePic.bottomAnchor),
+            labelText.leadingAnchor.constraint(equalTo: profilePic.trailingAnchor, constant: 8),
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
